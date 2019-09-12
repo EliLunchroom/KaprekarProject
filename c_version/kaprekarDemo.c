2605b686711ab8980712a8643479e110a59b39ad
Elijah Lundstrom <elundsrom2@huskers.unl.edu>
2019-09-12 15:49:33 -0500

    Update kaprekarDemo.c
    Made 'if' statement into 'else if' and made it look cleaner.

--- c_version/kaprekarDemo.c
+++ c_version/kaprekarDemo.c
@@ -1,18 +1,16 @@
 #include<stdlib.h>
 #include<stdio.h>
-
 #include "kaprekarUtils.h"
 
 int main(int argc, char **argv) {
 
+int n = atoi(argv[1]);
+
   if(argc != 2) {
-    fprintf(stderr, "Usage: %s n\n", argv[0]);
+    printf(stderr, "Usage: %s n\n", argv[0]);
     exit(1);
-  }
 
-  int n = atoi(argv[1]);
-
-  if(isKaprekar(n)) {
+  } else if(isKaprekar(n)) {
     printf("%d is a Kaprekar Number!\n", n);
   } else {
     printf("%d is not a Kaprekar Number!\n", n);
