85b9905aa3acfef08f623fb9aeab57119202061f
Elijah Lundstrom <elundsrom2@huskers.unl.edu>
2019-09-12 22:08:29 -0500

    Update kaprekarUtils.c
    changed int to double

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -33,7 +33,7 @@ int isKaprekar(int n) {
   long square = n * n;
 
   //compute the number of digits of the square
-  int numDigits =log10(square) + 1;
+  double numDigits =log10(square) + 1;
   long modulus = 1;
   long first, second;
 
