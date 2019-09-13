efd57d79dfec5a513e087c3a776a8f6576edf23f
Elijah Lundstrom <elundsrom2@huskers.unl.edu>
2019-09-12 20:11:53 -0500

    Update kaprekarUtils.c
    Made a new double

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -35,7 +35,7 @@ int isKaprekar(int n) {
   int numDigits = (int) log10(n) + 1;
   long modulus = 0;
   long first, second;
-
+  double numberOfDigits;
   //for each possible "split" of the square...
   for(i=1; i<=numberOfDigits; i++) {
     //increase the modulus by a factor of 10
