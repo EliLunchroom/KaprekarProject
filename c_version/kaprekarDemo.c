57b2913257f3c18fbf2cf1a4b9864c4f4301a54e
Elijah Lundstrom <elundsrom2@huskers.unl.edu>
2019-09-12 19:55:42 -0500

    Update kaprekarDemo.c
    Returned to original.

--- c_version/kaprekarDemo.c
+++ c_version/kaprekarDemo.c
@@ -1,20 +1,22 @@
 #include<stdlib.h>
 #include<stdio.h>
+
 #include "kaprekarUtils.h"
 
 int main(int argc, char **argv) {
 
-int n = atoi(argv[1]);
-
   if(argc != 2) {
-    printf(stderr, "Usage: %s n\n", argv[0]);
+    fprintf(stderr, "Usage: %s n\n", argv[0]);
     exit(1);
+  }
 
-  } else if(isKaprekar(n)) {
+  int n = atoi(argv[1]);
+
+  if(isKaprekar(n)) {
     printf("%d is a Kaprekar Number!\n", n);
   } else {
     printf("%d is not a Kaprekar Number!\n", n);
   }
 
-  return 0;
-}
+    return 0;
+  }
